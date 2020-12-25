884161d0aff6afe09032f440205fa7e5ae409e46
Mohamed Bouchnafa <mohamed.bouchnafa@babel.es>
2020-12-25 12:24:51 +0100

    add task 4 : add pull to refresh and error handling

--- app/src/main/java/com/example/githubbam/ListRepositoryActivity.kt
+++ app/src/main/java/com/example/githubbam/ListRepositoryActivity.kt
@@ -1,8 +1,10 @@
 package com.example.githubbam
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.lifecycleScope
 import com.example.githubbam.adapters.ListRepositoryAdapter
 import com.example.githubbam.presenters.ListRepositoryController
@@ -10,6 +12,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.lang.Exception
 
 class ListRepositoryActivity : AppCompatActivity() {
     private lateinit var listRepositoryController : ListRepositoryController
@@ -20,11 +23,35 @@ class ListRepositoryActivity : AppCompatActivity() {
         listRepositoryController= ListRepositoryController()
         listRepositoryAdapter= ListRepositoryAdapter(this)
         repoListView.adapter=listRepositoryAdapter
+        fetchListRepository()
+        swipeLayout.setOnRefreshListener {
+            fetchListRepository()
+        }
+    }
+
+    private fun fetchListRepository(){
         lifecycleScope.launch {
             loadingView.visibility= View.VISIBLE
-            val listRepo = listRepositoryController.getListRepository()
-            loadingView.visibility= View.GONE
-            listRepositoryAdapter.update(listRepo)
+            try {
+                val listRepo = listRepositoryController.getListRepository()
+                listRepositoryAdapter.update(listRepo)
+            }catch (exception : Exception){
+                showDialog(exception)
+            }finally {
+                loadingView.visibility= View.GONE
+                swipeLayout.isRefreshing=false
+            }
         }
+    }
+
+    private fun showDialog(exception: Exception){
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage(exception.localizedMessage)
+            .setPositiveButton(R.string.btn_ok
+            ) { dialog, id ->
+                finish()
+            }
+        // Create the AlertDialog object and return it
+        builder.create().show()
     }
 }
